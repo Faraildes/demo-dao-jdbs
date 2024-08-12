fde3497b83c765fd982dea78c8ac2ece826616eb
Faraildes <faraildesbarbosaferreira@gmail.com>
2024-08-12 17:02:06 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import db.DB;
+import db.DbIntegrityException;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Connection conn = null;
+		Statement st = null;
+		try {
+			conn = DB.getConnection();
+	
+						int rowsAffected = st.executeUpdate();
+			
+			System.out.println("Done! Rows affected: " + rowsAffected);
+		}
+		catch (SQLException e) {
+			throw new DbIntegrityException(e.getMessage());
+		} 
+		finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+	}
+}
